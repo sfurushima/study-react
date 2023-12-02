fcbd3207ac35992d39f1be586c62914cf8e64317
sfurushima <sho.furushima.xyz999@gmail.com>
2023-12-02 18:33:33 +0900

    gitコミット

--- 04_react_basic/src/070_component_nest/start/Example.js
+++ 04_react_basic/src/070_component_nest/start/Example.js
@@ -4,6 +4,14 @@ const Example = () => {
   return (
     <div className="component">
       <h3>Hello Component</h3>
+      {/* // ul>li{item-$}*5のタブで表現できる */}
+      <ul>
+        <li>item-1</li>
+        <li>item-2</li>
+        <li>item-3</li>
+        <li>item-4</li>
+        <li>item-5</li>
+      </ul>
     </div>
   );
 };
