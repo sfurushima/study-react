bec682180b8a571ece562f469eb16bc28a770681
sfurushima <sho.furushima.xyz999@gmail.com>
2023-12-02 23:00:43 +0900

    Fragmentタグ

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,17 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    // JSXは1つの要素でしか返せないため、div, h3, pタグが並んでいるとエラーになる
+    // FragmentはHTMLに出力せず、まとめることができるタグ
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quos quibusdam, dolore minima mollitia consectetur eum ipsum a praesentium aspernatur corporis vero ex necessitatibus. Consequuntur assumenda alias nam explicabo non.</p>
+    </React.Fragment>
   );
 };
 
